8eb2315fe79c87810ed9f9f4c835772bffaa9d0c
John Fernandes <johndiegon@hotmail.com>
2021-02-27 17:20:01 -0300

    alteraando o retangulo de lugar

--- ColunaPronta/Model/CantoneiraGuardaCorpo.cs
+++ ColunaPronta/Model/CantoneiraGuardaCorpo.cs
@@ -16,6 +16,56 @@ namespace ColunaPronta.Model
         public CantoneiraGuardaCorpo(Point2d pontoInicial, Posicao posicao)
         {
 
+            Point2d p1, p2, p3, p4, p5, p6;
+            var settings = new Settings();
+            var lado = settings.CantoneiraLargura;
+            var espessura = settings.CantoneiraEspessura;
+
+            var retangulo = 
+
+            switch (posicao)
+            {
+                case Posicao.BaixoDireita:
+                    p1 = new Point2d(pontoInicial.X, pontoInicial.Y);
+                    p2 = new Point2d(pontoInicial.X, pontoInicial.Y - (lado ));
+                    p3 = new Point2d(pontoInicial.X - ( lado ), pontoInicial.Y - (lado ));
+                    p4 = new Point2d(pontoInicial.X - ( lado ), pontoInicial.Y - ((lado - espessura ) ));
+                    p5 = new Point2d(pontoInicial.X - (espessura), pontoInicial.Y - ((lado - espessura ) ));
+                    p6 = new Point2d(pontoInicial.X - ( espessura ), pontoInicial.Y);
+                    break;
+                case Posicao.BaixoEsquerda:
+                    p1 = new Point2d(pontoInicial.X, pontoInicial.Y);
+                    p2 = new Point2d(pontoInicial.X, pontoInicial.Y - (lado ));
+                    p3 = new Point2d(pontoInicial.X + ( lado ), pontoInicial.Y - (lado ));
+                    p4 = new Point2d(pontoInicial.X + ( lado ), pontoInicial.Y - ((lado - espessura ) ));
+                    p5 = new Point2d(pontoInicial.X + ( espessura ), pontoInicial.Y - ((lado - espessura) ));
+                    p6 = new Point2d(pontoInicial.X + (espessura), pontoInicial.Y);
+                    break;
+                case Posicao.CimaDireita:
+                    p1 = new Point2d(pontoInicial.X, pontoInicial.Y);
+                    p2 = new Point2d(pontoInicial.X + ( lado ), pontoInicial.Y);
+                    p3 = new Point2d(pontoInicial.X + ( lado ), pontoInicial.Y - (lado ));
+                    p4 = new Point2d(pontoInicial.X + (( lado - espessura ) ), pontoInicial.Y - ((lado) ));
+                    p5 = new Point2d(pontoInicial.X + (( lado - espessura ) ), pontoInicial.Y - (espessura));
+                    p6 = new Point2d(pontoInicial.X, pontoInicial.Y - (espessura));
+                    break;
+                default: // Posicao.CimaEsquerda
+                    p1 = new Point2d(pontoInicial.X, pontoInicial.Y);
+                    p2 = new Point2d(pontoInicial.X, pontoInicial.Y - (lado ));
+                    p3 = new Point2d(pontoInicial.X + ( espessura ), pontoInicial.Y - (lado ));
+                    p4 = new Point2d(pontoInicial.X + (espessura), pontoInicial.Y - (espessura));
+                    p5 = new Point2d(pontoInicial.X + (lado ), pontoInicial.Y - (espessura));
+                    p6 = new Point2d(pontoInicial.X + (lado ), pontoInicial.Y);
+                    break;
+            }
+
+            PontosL.Add(p1);
+            PontosL.Add(p2);
+            PontosL.Add(p3);
+            PontosL.Add(p4);
+            PontosL.Add(p5);
+            PontosL.Add(p6);
+
         }
     }
 
